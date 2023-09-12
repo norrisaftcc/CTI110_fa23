33fa45ce606e08c874ab71a90856406a5797764d
teacherbot <norrisa@faytechcc.edu>
2023-09-12 13:54:05 -0400

    First version of the pokeman graph

---
+++ P2T1_norris.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+
+# Collect the data
+data = [] # empty list
+print("Enter Pokeman data:")
+print("Day 1: ", end="")
+day1 = int(input())
+print("Day 2: ", end="")
+day2 = int(input())
+print("Day 3: ", end="")
+day3 = int(input())
+
+# put the data in a list
+data = [day1, day2, day3]
+
+# TODO: Graph the real data
+fig, ax = plt.subplots()
+ax.plot([1, 2, 3], data)
+plt.title("Pokeman Data")
+plt.xlabel('day #')
+plt.ylabel('pokemans collected')
+plt.show()
